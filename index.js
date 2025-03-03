e5928cf6c02eec0539840d08d84c147fc3e71787
lishuanghai <lishuanghai@csii.com.cn>
2025-03-03 19:35:43 +0800

    update index.js

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // 导入 express 模块
 const express = require('express');
 
+//导入cors跨域中间件
+const cors = require("cors")
+
+const mysql = require ('mysql')
+
 // 创建一个 express 应用程序
 const app = express();
 
+// 全局注册，加前缀
+app.use(cors())
+
+//创建连接
+// let conn=mysql.createConnection({
+//   //主机地址
+//   host:'localhost',
+//   //用户名
+
+//   user:'root',
+//   //密码
+//   password:'960208',
+//   //数据库名称
+//   database:'xsgl'
+// })
+
+// conn.connect((err)=>{
+//   if(err) throw err;
+//   console.log('连接成功');
+// })
+
 // 定义一个路由处理程序，用于处理根路径的 GET 请求
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Hello, World!');
 });
+
+app.post('/testpost', (req, res) => {
+  res.send('posssss')
+})
 
 // 启动应用程序
 app.listen(3000, () => {
